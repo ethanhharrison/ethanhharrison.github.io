59951807d0fbb875218c8ec4ab364972a4506750
ethanhharrison <ethan.harrison111111@gmail.com>
2022-05-26 21:59:31 -0700

    Added Contact Form (No JS Yet)

--- js/script.js
+++ js/script.js
@@ -8,6 +8,6 @@ function openFumo(evt, fumoName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace("open", "closed");
     }
-    document.getElementById(fumoName).style.display = "block";
+    document.getElementById(fumoName).style.display = "flex";
     evt.currentTarget.className = evt.currentTarget.className.replace("closed", "open");
 }
